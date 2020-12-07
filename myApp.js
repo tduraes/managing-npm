b13569f89809cf3094aba4cfe8566e0827b4e0bd
TGD9 <duraes.tiago@gmail.com>
2020-12-07 15:25:49 +0000

    add a json route

--- myApp.js
+++ myApp.js
@@ -12,4 +12,8 @@ app.get("/",function(req, res) {
 app.use(express.static(__dirname + '/public'));
 app.use('/public', express.static(__dirname + '/public'));
 
+app.get("/json", function(req, res) {
+	res.json({"message":"Hello json"})
+})
+
  module.exports = app;
