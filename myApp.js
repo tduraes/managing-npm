0f04cc3a39c67417ef5fc612033d14c02fd919bb
TGD9 <duraes.tiago@gmail.com>
2020-12-07 15:50:34 +0000

    added .env file

--- myApp.js
+++ myApp.js
@@ -2,18 +2,29 @@ var express = require('express');
 var app = express();
 var bGround = require('fcc-express-bground');
 
+require('dotenv').config();
+
 console.log('Hello World');
 
 
-app.get("/",function(req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 })
 
 app.use(express.static(__dirname + '/public'));
 app.use('/public', express.static(__dirname + '/public'));
-
-app.get("/json", function(req, res) {
+/*
+app.get("/json", (req, res) => {
 	res.json({"message":"Hello json"})
+})*/
+
+app.get("/json", (req, res) => {
+	if (process.env.MESSAGE_STYLE === "uppercase") {
+		res.json({"message" : "HELLO JSON" })
+	} else {
+		res.json(
+			{"message" : "Hello json"})
+	}
 })
 
  module.exports = app;
