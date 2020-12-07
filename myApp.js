7083e39b151ed395f5f24965dba70b42ef268457
TGD9 <duraes.tiago@gmail.com>
2020-12-07 16:12:08 +0000

    added middleware logger

--- myApp.js
+++ myApp.js
@@ -4,6 +4,13 @@ var bGround = require('fcc-express-bground');
 
 require('dotenv').config();
 
+
+app.use((req, res, next) => {
+	console.log(req.method + " " + req.path + " - " + req.ip);
+	next();
+})
+
+
 console.log('Hello World');
 
 
